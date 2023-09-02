62c2092a45e95c1baa38aef5dd210ca7b38d424b
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 19:29:39 -0700

    second attempt

--- Solution.java
+++ Solution.java
@@ -1,16 +1,28 @@
 public class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n > flowerbed.length)
+            return false;
+        if (flowerbed.length == 1 && flowerbed[0] == 0 && n == 1)
+            return true;
+        
         int count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
+            
+            if (i == 0 && i + 1 < flowerbed.length) {
+                if ((flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                    count++;
+            }
+                
+            if ((i >= 1) && (i + 1 < flowerbed.length)){
+                if ((flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                    count++;
+            } 
 
-            if ((i == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
-                count++;
-
-            if ((i >= 1) && (flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
-                count++;
-
-            if ((i == flowerbed.length - 1) && (flowerbed[i] == 0) && (flowerbed[i - 1] == 0))
-                count++;
+            if (i == flowerbed.length - 1) {
+                if ((flowerbed[i] == 0) && (flowerbed[i - 1] == 0))
+                    count++;
+            }
+                
         }
         if (count >= n)
             return true;
