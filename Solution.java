5ee8c79a31a730ee6d2ab1bf94a51ab17d55b449
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 19:17:12 -0700

    first attempt, error encountered

--- Solution.java
+++ Solution.java
@@ -1,4 +1,20 @@
 public class Solution {
-public boolean canPlaceFlowers(int[] flowerbed, int n) {
-} 
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if ((i == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                count++;
+
+            if ((i >= 1) && (flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                count++;
+
+            if ((i == flowerbed.length - 1) && (flowerbed[i] == 0) && (flowerbed[i - 1] == 0))
+                count++;
+        }
+        if (count >= n)
+            return true;
+        else    
+            return false;
+    } 
 }
