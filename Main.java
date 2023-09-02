da9a3b38f06dace505fd7b4ec7965c9c55061c4f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 19:09:14 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.canPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1));
+        System.out.println(sol.canPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 2));
+        System.out.println(sol.canPlaceFlowers(new int[] { 0, 0, 1, 0, 0 }, 1));
+        System.out.println(sol.canPlaceFlowers(new int[] { 1 }, 1));
+        System.out.println(sol.canPlaceFlowers(new int[] {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0}, 4));
+    }
 }
