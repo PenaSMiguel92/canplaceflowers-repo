34d47d28c01719facc445e8b7758c9d6050b6c6e
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 19:39:08 -0700

    third attempt, includes persistance

--- Solution.java
+++ Solution.java
@@ -9,18 +9,27 @@ public class Solution {
         for (int i = 0; i < flowerbed.length; i++) {
             
             if (i == 0 && i + 1 < flowerbed.length) {
-                if ((flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                if ((flowerbed[i] == 0) && (flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
                     count++;
+                }
+                    
             }
                 
             if ((i >= 1) && (i + 1 < flowerbed.length)){
-                if ((flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                if ((flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
                     count++;
+                }
+                    
             } 
 
             if (i == flowerbed.length - 1) {
-                if ((flowerbed[i] == 0) && (flowerbed[i - 1] == 0))
+                if ((flowerbed[i] == 0) && (flowerbed[i - 1] == 0)) {
+                    flowerbed[i] = 1;
                     count++;
+                }
+                    
             }
                 
         }
